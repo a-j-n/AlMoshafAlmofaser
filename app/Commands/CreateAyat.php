5f2de2461399492cc122d7a2fbada38658109182
J <e.a.gamal@gmail.com>
2019-04-10 23:29:44 +0200

    create images

---
+++ app/Commands/CreateAyat.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Commands;
+
+use App\QuranText;
+use Intervention\Image\ImageManagerStatic as Image;
+use LaravelZero\Framework\Commands\Command;
+
+class CreateAyat extends Command
+{
+
+    protected $signature = 'image:create';
+
+
+    protected $description = 'Create Images with quran Text';
+
+    public function handle()
+    {
+        $ayat = QuranText::all();
+        $bar = $this->output->createProgressBar(count($ayat));
+        foreach ($ayat as $aya) {
+            $img = Image::canvas(400, 200, '#000');
+            $img->text($aya->text, 0, 0, function ($font) {
+                $font->file('public/DroidKufi/DroidKufi-Bold.ttf');
+                $font->size(24);
+                $font->color('#fff');
+                $font->align('center');
+                $font->valign('top');
+                $font->angle(0);
+            });
+            $img->save(public_path($aya->sura . '_' . $aya->aya . '.jpg'), 80);
+
+            $bar->advance();
+
+        }
+        $bar->finish();
+    }
+
+
+}
---
+++ app/QuranText.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QuranText extends Model
+{
+    protected $table= 'quran_text';
+    protected $primaryKey = 'index';
+}
